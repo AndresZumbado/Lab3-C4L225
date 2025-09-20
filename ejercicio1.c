65281ab270f7bd1730378aa6af2782513415b3bf
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-20 12:11:27 -0600

    Se modifico el codigo para que ahora el usuario pueda introducir los dos numeros para calcular el factorial

--- ejercicio1.c
+++ ejercicio1.c
@@ -10,8 +10,16 @@ int factorial (int n) {
 }
 
 int main (int arge, char *argv[]) {
-	int fac4 = factorial (4);
-	int fac5 = factorial (5);
-	printf("4! = %d, 5! = %d\n", fac4, fac5);
+	printf("Este programa le ayuda a calcular el factorial de dos numeros que ingrese\n");
+	
+	int numero1;
+	int numero2;
+	printf ("Ingrese el primer numero: ");
+	scanf ("%d", &numero1);
+	printf ("Ingrese el segundo numero: ");
+	scanf ("%d", &numero2);
+	int fac4 = factorial (numero1);
+	int fac5 = factorial (numero2);
+	printf("%d! = %d, %d! = %d\n", numero1, fac4, numero2, fac5);
 	return 0;
 }
