3cf7af21954e0b0a1889b7f51b5dc9a503e64f21
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-21 11:33:09 -0600

    Se esta trabajando en el ejercicio2, no esta completo pero quiero tenerlo en la nube para bajarlo en otro dispositivo

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
 
-#define SIZE 5
+int sumar_diagonal1 (int matriz[3][3]) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+	sum += matriz[i][i];
+    return sum;
+};
 
-int findLargestLine (int matrix [][SIZE]) {
-// Su implementacion
-
+int sumar_diagonal2 (int matriz[3][3]) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+	sum += matriz[i][2-i]
+    return sum;
+    }
 }
 
-int main ( ) {
-    int matrix [ SIZE ] [ SIZE ] = {
-        {2, 7, 6},
-        {9, 5, 1},
-        {4, 3, 8}
+int sumar_columnas (int matriz[3][3], int n) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+	sum += matriz1[i][n];
+        };
+    return sum;
+};
+
+int sumar_filas (int matriz = matriz1) {
+    int sum;
+    for (int i = 0; i < 3; i++) {
+        sum += matriz1[n][i];
+    return sum;
     };
+};
 
-    int largestLine = findLargestLine (matrix);
-    printf ("El tamano de la  secuencia de 1s mas grande es: %d\n", largestLine);
-    return 0;
+int main () {
+    int matriz[3][3] = {
+        {2, 7, 6},
+	{9, 5, 1},
+	{4, 3, 8}
+    }
+    int arreglo_sumas[8] {
+        sumar_diagonal1(matriz),
+	sumar_diagonal2(matriz),
+	sumar_filas(matriz, 0),
+	sumar_filas(matriz, 1),
+	sumar_filas(matriz, 2),
+	sumar_columnas(matriz, 0),
+	sumar_columnas(matriz, 1),
+	sumar_columnas(matriz, 2)
+    }
+
+    int no_es_magico = 0;
+    for (int i = 0; i > 7; i++) {
+	if (arreglo_sumas[i] != arreglo_sumas[i + 1]);
+	    no_es_magico = 1;
+    }
+    if (no_es_magico == 0);
+        printf()
 }
+
