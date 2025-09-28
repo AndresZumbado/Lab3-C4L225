aa8c6150ecabeb107118e5b21d596ee177747f49
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-27 18:26:43 -0600

    Se le agregaron comentarios al ejercicio y semodifico la apariencia de la estructura del codigo

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//Se define la funcion factorial, esta recibe un numero entero, calcula su factorial y devuelve el factorial
 int factorial (int n) {
 	int i = 1;
 	while (n > 1) {
@@ -9,15 +9,19 @@ int factorial (int n) {
 	return i;
 }
 
+
 int main (int arge, char *argv[]) {
-	printf("Este programa le ayuda a calcular el factorial de dos numeros que ingrese\n");
-	
+	printf("Este programa le ayuda a calcular el factorial de dos numeros que ingrese\n");	
+
+	//Se inicializan las variables correspondientes a los numeros que entrega el usuario. Posteriormente se les asigna un valor usando scanf.
 	int numero1;
 	int numero2;
 	printf ("Ingrese el primer numero: ");
 	scanf ("%d", &numero1);
 	printf ("Ingrese el segundo numero: ");
 	scanf ("%d", &numero2);
+
+	//Se llama a la funcion factorial para obtener ek factorial de ambos numeros
 	int fac4 = factorial (numero1);
 	int fac5 = factorial (numero2);
 	printf("%d! = %d, %d! = %d\n", numero1, fac4, numero2, fac5);
