bc4de6a789147f9471721ac0d5001dbbf4a69f0b
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-27 21:22:43 -0600

    Se modifico el codigo para que trabajara con un macro SIZE y de esa forma permitir matrices con size diferente a 3.

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,76 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 6
 
 
-void generar_matriz(int matriz[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+void generar_matriz(int matriz[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             matriz[i][j] = rand() % 10;
         }
     }
 }
 
-int sumar_diagonal1(int matriz[3][3]) {
+int sumar_diagonal1(int matriz[SIZE][SIZE]) {
     int sum = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < SIZE; i++) {
         sum += matriz[i][i];
     }
     return sum;
 } 
 
-int sumar_diagonal2(int matriz[3][3]) {
+int sumar_diagonal2(int matriz[SIZE][SIZE]) {
     int sum = 0;
-    for (int i = 0; i < 3; i++) {
-        sum += matriz[i][2 - i];
+    for (int i = 0; i < SIZE; i++) {
+        sum += matriz[i][SIZE - 1 - i];
     }
     return sum;
 }
 
-int sumar_columnas(int matriz[3][3], int n) {
+int sumar_columnas(int matriz[SIZE][SIZE], int n) {
     int sum = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < SIZE; i++) {
         sum += matriz[i][n];
     }
     return sum;
 }
 
-int sumar_filas(int matriz[3][3], int n) {
+int sumar_filas(int matriz[SIZE][SIZE], int n) {
     int sum = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < SIZE; i++) {
         sum += matriz[n][i];
     }
     return sum;
 }
 
 int main() {
-    int matriz[3][3];
+    int matriz[SIZE][SIZE];
     generar_matriz(matriz);
 
     printf("La matriz utilizada corresponde a:\n");
-    for (int i = 0; i < 3; i++) {
-	for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < SIZE; i++) {
+	for (int j = 0; j < SIZE; j++) {
 	    printf("%d ", matriz[i][j]);
 	};
 	printf("\n");
     };
 
-    int arreglo_sumas[8] = {
-        sumar_diagonal1(matriz),
-        sumar_diagonal2(matriz),
-        sumar_filas(matriz, 0),
-        sumar_filas(matriz, 1),
-        sumar_filas(matriz, 2),
-        sumar_columnas(matriz, 0),
-        sumar_columnas(matriz, 1),
-        sumar_columnas(matriz, 2)
-    };
+    int arreglo_sumas[SIZE*2 + 2];
+        arreglo_sumas[0] = sumar_diagonal1(matriz);
+        arreglo_sumas[1] = sumar_diagonal2(matriz);
+        for (int i = 0; i < SIZE; i++) {
+	    arreglo_sumas[i+2] = sumar_filas(matriz, i);
+	}
+	for (int i = 0; i < SIZE; i++) {
+	    arreglo_sumas[i+2+SIZE] = sumar_filas(matriz, i);	
+	}
 
     int no_es_magico = 0;
-    for (int i = 0; i < 8; i++) {
-        //printf("%d\n", arreglo_sumas[i]);
-    };
 
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < SIZE*2 + 2 -1; i++) {
         if (arreglo_sumas[i] != arreglo_sumas[i + 1]) {
             no_es_magico = 1;
         };
